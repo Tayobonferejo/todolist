a338d6bdf467752ebabf5c320501008a43a053e7
tayobonferejo <oluwatayogeorgeige@gmail.com>
2026-07-10 14:40:52 +0100

    frontend

--- todo/src/Home.jsx
+++ todo/src/Home.jsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Create  from './Create.jsx'
 
 function Home() {
+    const [todos ,setTodos] = useState([])
     return (
         <div>
             <h2>todo list</h2>
+            <Create></Create>
+            {
+                todos.length === 0
+                ?
+                <div><h2>No Record</h2></div>
+                :
+                todos.map(todo => {
+                    <div>
+                        {todo}
+                    </div>
+                })
+            }
         </div>
     )
 }
 
-export default Home 
+export default Home  
